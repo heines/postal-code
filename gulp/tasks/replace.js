263792b5d37a50028bb8da7357d61e07661056f2
清水ハイネ <heine.shimizu@gmail.com>
2019-06-09 01:09:25 +0900

    github pages 設定対応

--- gulp/tasks/replace.js
+++ gulp/tasks/replace.js
@@ -5,8 +5,8 @@ const DIR = require('../conf').DIR;
 const conf = require('../conf').replace;
 
 gulp.task('replaceHtml', () => {
-  const regJs = new RegExp(`(src="${DIR.PATH}\/js\/)([a-z0-9_\.\-]*)(\.js")`);
-  const regCss = new RegExp(`(href="${DIR.PATH}\/css\/)([a-z0-9_\.\-]*)(\.css")`);
+  const regJs = new RegExp(`(src=".*\/js\/)([a-z0-9_\.\-]*)(\.js")`);
+  const regCss = new RegExp(`(href=".*\/css\/)([a-z0-9_\.\-]*)(\.css")`);
   return gulp.src(conf.html.src)
     .pipe($.replace(regJs, '$1$2.min$3'))
     .pipe($.replace(regCss, '$1$2.min$3'))
